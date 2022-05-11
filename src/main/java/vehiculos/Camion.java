020cd1d602a4922f2b3776cc3f31fb3edf7b8f43
Keder <Kmaderap@unal.edu.co>
2022-05-11 13:06:02 -0500

    .

---
+++ src/main/java/vehiculos/Camion.java
@@ -0,0 +1,26 @@
+package vehiculos;
+
+public class Camion extends Vehiculo{
+
+    private static int cantidadCamiones;
+    private int ejes;
+
+    public Camion(String placa, String nombre, float precio, float peso, Fabricante fabricante, int ejes){
+        super(placa, 2, 80, nombre, precio, peso, "4X2", fabricante);
+        this.ejes = ejes;
+        Camion.cantidadCamiones++;
+    }
+
+    public void setEjes(int ejes){
+        this.ejes = ejes;
+    }
+
+    public int getEjes(){
+        return ejes;
+    }
+
+    public static int getCantidadCamiones(){
+        return Camion.cantidadCamiones;
+    }
+}
+
